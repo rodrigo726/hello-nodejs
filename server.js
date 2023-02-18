2e6e092948030f5e0d29c5dd78dc268f7d2d3865
Rodrigo Acuña <90769578+rodrigo726@users.noreply.github.com>
2023-02-18 11:23:07 -0500

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  res.end('HOLA MUNDO :D');
 });
 
 server.listen(port, () => {
